1ff3e0c54efbc0ca5f208f5adae326c0e8dfe458
Mit Parekh <35421311+MitP97@users.noreply.github.com>
2018-01-16 10:24:32 +0530

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -4,8 +4,8 @@
  * All database connection variables
  */
  
-define('DB_USER', "root"); // db user
-define('DB_PASSWORD', "mit97"); // db password (mention your db password here)
-define('DB_DATABASE', "eye2"); // database name
-define('DB_SERVER', "localhost"); // db server
+define('DB_USER', "USER"); // db user
+define('DB_PASSWORD', "PASS"); // db password (mention your db password here)
+define('DB_DATABASE', "DBNAME"); // database name
+define('DB_SERVER', "DBSERVER"); // db server
 ?>
